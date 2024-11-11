457114bbc79309b472313b3eef7635f9a32a32ac
Noah <holtken1@mymail.nku.edu>
2024-11-11 15:18:59 -0500

    Update load.py

--- scraper/src/load.py
+++ scraper/src/load.py
@@ -1,7 +1,16 @@
-# from pymongo import MongoClient
-# import json
-#
-#
-# def load_competition_data():
-#     client = MongoClient()
-#     db = client["lucky_scarf"]
+from pymongo import MongoClient
+
+
+def load_competition(comp_dict, comp_code, sub_code):
+    client = MongoClient()
+    db = client["lucky_scarf"]
+
+    if sub_code == "MTL":
+        for match_dict in comp_dict["matches"]:
+            db[f"{comp_code}_matchlist"].insert_one(match_dict)
+    elif sub_code == "STN":
+        for standings_dict in comp_dict["standings"][0]["table"]:
+            db[f"{comp_code}_standings"].insert_one(standings_dict)
+    elif sub_code == "TML":
+        for team_dict in comp_dict["teams"]:
+            db[f"{comp_code}_teamlist"].insert_one(team_dict)
