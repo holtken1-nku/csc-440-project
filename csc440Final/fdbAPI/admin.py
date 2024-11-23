d2c185a60442347dbc045717e76354043d26be4c
nreedschool <reedn3@nku.edu>
2024-11-23 18:05:03 -0500

    Delete csc440Final directory

--- csc440Final/fdbAPI/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Competitions,Teams,Matches,Standings
-
-
-admin.site.register(Competitions)
-admin.site.register(Teams)
-admin.site.register(Matches)
-admin.site.register(Standings)
--- csc440Final/fdbAPI/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FdbapiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fdbAPI'
